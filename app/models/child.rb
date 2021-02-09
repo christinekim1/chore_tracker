0f198259d237c51d2d71a8d2ec15bc6bac5e8f03
Christine Kim <christi4@andrew.cmu.edu>
2021-02-08 20:00:54 -0500

    validations passed

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,3 +1,11 @@
 class Child < ApplicationRecord
-    
+    has_many :chores
+    has_many :tasks, through: :chores
+
+    validates_presence_of :first_name, :last_name
+
+    # validates :first_name, presence: true
+    # validates :last_name, presence: true
+
+
 end
